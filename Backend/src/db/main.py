f4507aa2b11e69801d6b338e5d7b74dacb8c2cb9
kajal-jotwani <kajaljotwani06@gmail.com>
2025-09-08 08:24:46 +0530

    feat: Add async database session management

--- Backend/src/db/main.py
+++ Backend/src/db/main.py
@@ -1,6 +1,7 @@
 from sqlmodel import create_engine, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlmodel.ext.asyncio.session import AsyncSession 
+from sqlalchemy.orm import sessionmaker
 
 from src.config import Config
 
@@ -22,3 +23,13 @@ async def init_db():
         # Create all tables in the database
         await conn.run_sync(SQLModel.metadata.create_all)
     print("Database initialized successfully.")
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+
+    async with Session() as session:
+        yield session
