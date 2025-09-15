e5e3154e2f66def454674724f5cdac45e640c5ff
kajal-jotwani <kajaljotwani06@gmail.com>
2025-09-16 00:17:40 +0530

    feat: Add custom logging and CORS middleware

--- Backend/src/__init__.py
+++ Backend/src/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.db.main import init_db
 from contextlib import asynccontextmanager
 from src.auth.routes import auth_router 
+from .middleware import register_middleware
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -18,5 +19,7 @@ app = FastAPI(
     version=version,
     lifespan=life_span
 )
+
+register_middleware(app)
 
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
